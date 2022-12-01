b31b74edd0ac9d00bcfe88f440ec7a68304bac36
David R. Law <dlaw@stsci.edu>
2022-12-01 14:55:50 -0500

    Add p_subarray keyword to filter offset ref files

--- miricoord/imager/makecrds/makecrds_ima_flt2.py
+++ miricoord/imager/makecrds/makecrds_ima_flt2.py
@@ -132,18 +132,23 @@ def make_filter_offset(distfile, outname, subarray):
     if (subarray == 'MASK1065'):
         extname = 'BoresightCORON1065'
         subvalues = "MASK1065"
+        psubvalues = "MASK1065"
     elif (subarray == 'MASK1140'):
         extname = 'BoresightCORON1140'
         subvalues = "MASK1140"
+        psubvalues = "MASK1140"
     elif (subarray == 'MASK1550'):
         extname = 'BoresightCORON1550'
         subvalues = "MASK1550"
+        psubvalues = "MASK1550"
     elif (subarray == 'MASKLYOT'):
         extname = 'BoresightCORONLYOT'
         subvalues = "MASKLYOT"
+        psubvalues = "MASKLYOT"
     else:
         extname = 'Boresight offsets'
-        subvalues = "FULL|BRIGHTSKY|SUB256|SUB128|SUB64|SLITLESSPRISM|"
+        subvalues = "FULL"
+        psubvalues = "FULL|BRIGHTSKY|SUB256|SUB128|SUB64|SLITLESSPRISM|"
         
     with fits.open(distfile) as f:
         data = f[extname].data
@@ -160,6 +165,7 @@ def make_filter_offset(distfile, outname, subarray):
     model.meta.description = "FLT2 delivery"
     model.meta.input_units = "pixels"
     model.meta.instrument.subarray = subvalues
+    model.meta.p_subarray = psubvalues
 
     for item in data:
         model.filters = d
